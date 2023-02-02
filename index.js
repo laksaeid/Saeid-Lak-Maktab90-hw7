17e3f61992fa3cfe79eaa94e9818c0d210f9b628
saeid lak <laksaeidd2@gmail.com>
2023-02-02 08:33:12 +0330

    bug fix

--- index.js
+++ index.js
@@ -9,11 +9,8 @@ let salaries = {
 const getTop = function (salari) {
   let array = Object.entries(salari);
   array.sort((a, b) => b[1] - a[1]);
-  const top = array.filter((value) => value[1] === array[0][1]);
-  const [[name, mostSalari], ...other] = top;
-  let reachests = array.filter((value) => {
-    return value[1] === mostSalari;
-  });
-  return reachests;
+  const reachests = array.filter((value) => value[1] === array[0][1]);
+  const [[name, mostSalari], ...other] = reachests;
+  return top;
 };
 console.log(getTop(salaries).join(" \n"));
