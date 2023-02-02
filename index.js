bad2df85a51d1dbf168a26c9de130806f93aa2ab
saeid lak <laksaeidd2@gmail.com>
2023-02-02 08:27:04 +0330

    add gitignore

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 "use strict";
 
 let salaries = {
-  "John": 100,
-  "Pete": 300,
-  "Marssy": 250,
-  "Mary": 300,
+  John: 100,
+  Pete: 300,
+  Marssy: 250,
+  Mary: 300,
 };
 const getTop = function (salari) {
-  const reachests = [];
   let array = Object.entries(salari);
-  array.sort((a,b)=> b[1] - a[1])
-  const top = array.filter(value =>  value[1] === array[0][1])
-  for (const name of top) {
-    reachests.push(name[0]);
-  }
+  array.sort((a, b) => b[1] - a[1]);
+  const top = array.filter((value) => value[1] === array[0][1]);
+  const [[name, mostSalari], ...other] = top;
+  let reachests = array.filter((value) => {
+    return value[1] === mostSalari;
+  });
   return reachests;
-}
-console.log(getTop(salaries));
+};
+console.log(getTop(salaries).join(" \n"));
